d4a0d47c9d87b64fd566ad568aaa7e2477b4718b
Drew Caddell <dcaddell@invoca.com>
2019-04-19 18:25:36 -0700

    Add help command

--- events/message.js
+++ events/message.js
@@ -1,16 +1,8 @@
 module.exports = (client, message) => {
-  if (message.content.match(/^\/poll /)) {
-    params = message.content.split('poll ')
-
-    message.channel.send(`Should we ${params[1]}?`)
-      .then( response => {
-        response.react('👍')
-        response.react('👎')
-      })
-      .catch(() => console.error('One of the emojis failed to react.'))
-  }
-
   switch(message.content) {
+    case '/help': // TODO: Pull this out into an array somewhere
+      message.reply('Accepted commands are /tableflip, /shrug, /disappointed, /success, /poll <yes/no question>')
+      break;
     case 'ping':
       message.reply('Pong!')
       break;
@@ -27,5 +19,20 @@ module.exports = (client, message) => {
       message.channel.send("Noice", {files: ["./assets/256px-SuccessKid.jpg"]})
       break;
     // Leaving no default case for now. Probably nothing I want to happen on _every_ message sent
+  }
+
+  if (message.content.match(/^\/poll /)) {
+    simplePoll(message)
+  }
+
+  function simplePoll(message) {
+    params = message.content.split('poll ')
+
+    message.channel.send(`Should we ${params[1]}?`)
+      .then( response => {
+        response.react('👍')
+        response.react('👎')
+      })
+      .catch(() => console.error('One of the emojis failed to react.'))
   }
 }
