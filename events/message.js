323f1921f530d818ee0db68895b782fb8fda9b05
synhyborex <bhargavb92@gmail.com>
2019-04-23 15:23:18 -0700

    added some new features! still working on getting the bot to properly ping people

--- events/message.js
+++ events/message.js
@@ -1,5 +1,5 @@
 module.exports = (client, message) => {
-  switch(message.content) {
+  switch(message.content.toLowerCase()) {
     case '/help': // TODO: Pull this out into an array somewhere
       message.reply('Accepted commands are /tableflip, /shrug, /disappointed, /success, /poll <yes/no question>')
       break;
@@ -18,6 +18,54 @@ module.exports = (client, message) => {
     case '/success':
       message.channel.send("Noice", {files: ["./assets/256px-SuccessKid.jpg"]})
       break;
+    case '/pitchfork':
+      message.channel.send('---E')
+      break;
+    case '/praise':
+      message.channel.send('\\[T]/')
+      break;
+    case '/praisethesun':
+      message.channel.send('\\[T]/ PRAISE THE SUN \\[T]/')
+      break;
+    case '/upvote':
+    case '/approve':
+      message.channel.send('👍')
+      break;
+    case '/downvote':
+    case '/disapprove':
+      message.channel.send('👎')
+      break;
+    case '/fuckit':
+      message.channel.send("👍", {files: ["./assets/ObamaFuckItHaveAnUpvote.png"]})
+      break;
+    case 'murica':
+      message.channel.send("DID I HEAR FREEDOM RING?", {files: ["./assets/MURICA.jpg"]})
+      break;
+
+    // Chat member summons
+    case 'drew':
+    case 'dewsef':
+    case 'drewsef':
+      message.channel.send('@Drew we need you!')
+      break;
+    case 'haotian':
+    case 'ht':
+    case 'haoboy':
+      message.channel.send('@hwhwa#7123 we need you!')
+      break;
+    case 'george':
+    case 'shu':
+      message.channel.send('@George#4720 we need you!')
+      break;
+    case 'greg':
+    case 'greggers':
+      message.channel.send('@greggers#9291 we need you!')
+      break;
+    case 'bhargav':
+    case 'bman':
+      message.channel.send('@fortunesfavored#3293 we need you!')
+      break;
+
     // Leaving no default case for now. Probably nothing I want to happen on _every_ message sent
   }
 
